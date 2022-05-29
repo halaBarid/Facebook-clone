f04b72eb0fc251414884d9e182ef476f77fdda01
halaBarid <baridhala@gmail.com>
2022-05-29 14:37:20 +0100

    styling

---
+++ src/components/homeComponents/OnlineFriend.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OnlineFriend = () => {
+  return (
+    <div className='online-friend'>
+        <img src="https://scontent.fcmn5-2.fna.fbcdn.net/v/t1.6435-1/121692972_203462171146574_2856077478129318748_n.jpg?stp=cp0_dst-jpg_p80x80&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeGdObbGqie8vmAKwj41LRr5ikY6_oULtfGKRjr-hQu18QTAd5bag4y4iJP8llYKa4C9Q8SDdgZsOhAOJSmvRlLE&_nc_ohc=akh_P--Wz4MAX-7cSL8&_nc_ht=scontent.fcmn5-2.fna&oh=00_AT_qf3HUNzlPWpzWvsA1MZ_pjsVnN6byxmEYUjrjO1OWdA&oe=62B77440" alt="" />
+        <p>Jhon Bouloi</p>
+    </div>
+  )
+}
+
+export default OnlineFriend
--- src/components/homeComponents/ThirdHomeColumn.jsx
+++ src/components/homeComponents/ThirdHomeColumn.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
+import './thirdHomeColumn.css'
+import { BiDotsHorizontalRounded, BiSearch } from 'react-icons/bi'
+import { BsCameraVideoFill, BsGift } from 'react-icons/bs'
+import OnlineFriend from './OnlineFriend'
 
 const ThirdHomeColumn = () => {
   return (
-    <div className='col-md-4'>ThirdHomeColumn</div>
+    <div className='third-home-column col-md-4'>
+     <div className='friend-request'>
+        <div className='header'>
+          <h4>Friend request</h4>
+          <span><a href="">See all</a></span>
+        </div>
+        <div className='request-info'>
+            <div className='img-buttons'>
+              <div>
+                <img src="https://scontent.frak3-1.fna.fbcdn.net/v/t31.18172-1/13669391_1746996745583559_7572731138293938270_o.jpg?stp=c21.0.80.80a_cp0_dst-jpg_p80x80&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeFuhJBOxW7H61nZVrC9M_xVrRj9j0VMhnmtGP2PRUyGeUvLye6CPMRi1sNj2ze9XQt9zM51-2Dzawi52S5TyVUN&_nc_ohc=SaSXHfbL5Z8AX9yz3nz&_nc_ht=scontent.frak3-1.fna&oh=00_AT-L_M6y0PiFeObNqb2_bptVz-SBMUm1BqzNTERPvcB-pg&oe=62B704D4" alt="" />
+              </div>
+              <div className='buttons'>
+                <p>Jack Buch</p>
+                <button className='confirm'>Confirm</button>
+                <button className='delete'>Delete</button>
+              </div>
+            </div>
+          <div>
+            <div>3d</div>
+          </div>
+        </div>
+        
+     </div>
+    <hr />
+    <div className='birthday'>
+      <h4>Birthdays</h4>
+      <div className='birthday-info'>
+        <BsGift className='icon'/> <p><span>Joe Barid</span> have birthday today.</p>
+      </div>
+    </div>
+    <hr />
+    <div className='contact'>
+      <div className="contact-header">
+        <div>
+          <h4>Contact</h4>
+        </div>
+        <div>
+          <BsCameraVideoFill className='icon'/>
+          <BiSearch className='icon'/>
+          <BiDotsHorizontalRounded className='icon'/>
+        </div>
+      </div>
+      <div >
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+        <OnlineFriend/>
+      </div>
+    </div>
+    </div>
   )
 }
 
